1beec6fe6dbb238ea8f9631b194bfad5bf07daa3
Vito Sartori <hey@vito.io>
2024-08-20 19:29:21 -0300

    WIP

--- wsgo.c
+++ wsgo.c
@@ -13,7 +13,7 @@ typedef struct wsgo_state {
 
 int wsgo_init(int leds, int gpio, void **stateOut) {
     ws2811_t strip = {
-            .freq = 800000,
+            .freq = WS2811_TARGET_FREQ,
             .dmanum = 10,
             .channel = {
                     [0] = {
@@ -58,9 +58,9 @@ int wsgo_set_color(void *rawState, uint8_t r, uint8_t g, uint8_t b, uint8_t brig
     wsgo_state_t *state = (wsgo_state_t *)rawState;
     state->leds.channel[0].brightness = brightness;
     ws2811_led_t color = 0x00;
+    color |= r << 16;
     color |= g << 8;
-    color |= b << 16;
-    color |= r << 24;
+    color |= b;
 
     for (size_t i = 0; i < (size_t)state->leds.channel[0].count; i++) {
         state->leds.channel[0].leds[i] = color;
